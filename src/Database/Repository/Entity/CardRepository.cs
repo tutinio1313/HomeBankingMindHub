8cf325958131262e1fd90d4a4b7f0a7b4b721778
tutinio1313 <tutinio1313@gmail.com>
2024-02-10 07:20:04 -0300

    HotFix on validation

--- src/Database/Repository/Entity/CardRepository.cs
+++ src/Database/Repository/Entity/CardRepository.cs
@@ -6,9 +6,9 @@ namespace HomeBankingMindHub.Database.Repository;
     public class CardRepository(HomeBankingContext context) : Repository<Card>(context), ICardRepository
     {
         public bool ExistsCardByNumber(string number) => FindByCondition(x => x.Number == number).Any();
-        public bool CanPostNewCard(string clientID, CardType Type) => FindByCondition(x => x.ClientID == clientID)
+        public bool CanPostNewCard(string clientID, CardType? Type) => FindByCondition(x => x.ClientID == clientID)
                                                                     .Where(x => x.Type == (CardType)Type)
-                                                                    .Count() <= 3;
+                                                                    .Count() < 3;
         
     }
 
